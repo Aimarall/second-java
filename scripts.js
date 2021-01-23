47d8c91a2615d2e2295510c72b7d86022f0b1818
Aimarall <aimaral21072004@gmail.com>
2021-01-23 15:12:53 +0600

    added java

---
+++ scripts.js
@@ -0,0 +1,64 @@
+let first = "Aimaral";
+
+// alert(first);
+
+let second = first;
+
+//alert(second);
+
+//console.log(second);
+
+let third = second;
+
+//console.log(third);
+
+let fourth = third;
+
+//console.log(fourth);
+
+let fifth = fourth;
+
+//console.log(" My name is " + fifth);
+
+let city = "Bishkek";
+let country = "Kyrgyzstan";
+
+city = "Tashkent";
+
+//console.log( city + ` i'm is the capital of ` + country);
+//console.log( city + ' i\'m is the capital of '  + country);
+//console.log( city + " i'm is the capital of "  + country);
+
+
+let month = "january";
+let season = "winter";
+
+month = "May"
+season = "spring"
+
+//console.log(month + " is " + season);
+
+let song = "Yesterday";
+let author = "Beatles";
+
+song = "Giant"
+author = "Alla"
+
+console.log(" My favorite song is \"" + song + "\" by  \"" + author + "\"!");
+
+const PI = 3.14;
+const OUR_PLANET = "Earth";
+
+const SPEED_OF_LIGHT = 300000;
+
+let number = 1;
+let anotherNumber = 4.12;
+
+console.log(number + anotherNumber - 2 + 0.02 - -2 * 2);
+
+let a = +prompt("Какова ширина?");
+let b = +prompt("Какова высота?");
+
+let area = a * b;
+
+console.log(area);
